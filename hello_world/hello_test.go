566798fe224d39bf1bb4b85730868f302cf31783
rodrigojb <rodrigo.ballestero@mercadolibre.com>
2022-08-26 22:12:18 -0300

    Add code for the class 'Hello, World'

---
+++ hello_world/hello_test.go
@@ -0,0 +1,35 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	t.Run("saying hello to people", func(t *testing.T) {
+
+		got := Hello("Rodri", "")
+		want := "Hello, Rodri"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Eloide", "Spanish")
+		want := "Hola, Eloide"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Zinedine", "French")
+		want := "Bonjour, Zinedine"
+		assertCorrectMessage(t, got, want)
+	})
+
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
